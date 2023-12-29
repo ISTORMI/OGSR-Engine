094f54c1c1556bbcffbcdea9deb367da1964bc9d
Username3228 <75955587+ISTORMI@users.noreply.github.com>
2023-12-29 13:59:29 +0200

    Upd.

--- ogsr_engine/xr_3da/xr_ioc_cmd.cpp
+++ ogsr_engine/xr_3da/xr_ioc_cmd.cpp
@@ -694,6 +694,7 @@ void CCC_Register()
     //	CMD3(CCC_Mask,		"rs_disable_objects_as_crows",&psDeviceFlags,	rsDisableObjectsAsCrows	);
     CMD3(CCC_Mask, "rs_fullscreen", &psDeviceFlags, rsFullscreen);
     CMD3(CCC_Mask, "rs_stats", &psDeviceFlags, rsStatistic);
+    CMD4(CCC_Float, "rs_vis_distance", &psVisDistance, 0.3f, 3.0f);
 
     CMD3(CCC_Mask, "rs_cam_pos", &psDeviceFlags, rsCameraPos);
 #ifdef DEBUG
